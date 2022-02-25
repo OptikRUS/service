a8a3baabfe885cf59959fa028b079c27bc02ef64
optikrus <optikrus61@gmail.com>
2022-02-25 14:57:18 +0300

    improved users serializers and models

--- users/models.py
+++ users/models.py
@@ -7,3 +7,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     uid = models.UUIDField(primary_key=True, default=uuid4)
     email = models.EmailField('email address', blank=True, unique=True)
+
+    def __str__(self):
+        return f'Пользователь {self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
