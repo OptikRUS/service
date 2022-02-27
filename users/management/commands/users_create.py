79937b99ca367e02c5335d629f1d0e0a5a51e7db
optikrus <optikrus61@gmail.com>
2022-02-27 15:08:18 +0300

    improve management command users_create

--- users/management/commands/users_create.py
+++ users/management/commands/users_create.py
@@ -1,33 +1,46 @@
 from django.core.management.base import BaseCommand
+from django.db import transaction
+
+import factory
+from factory.django import DjangoModelFactory
 
 from users.models import User
 
 
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    email = factory.Faker('email')
+
+
 class Command(BaseCommand):
-    help = 'Create users to test'
+    help = 'Create superuser django & users to test'
 
     def add_arguments(self, parser):
         parser.add_argument('count', type=int)
 
+    @transaction.atomic
     def handle(self, *args, **options):
         User.objects.all().delete()
         count = options['count']
-        superuser = User(
-            # данные суперпользователя
+        superuser = User.objects.create_superuser(
             username='django',
             email='django@gb.local',
             first_name='Джанго',
             last_name='Фреймворков',
+            password='geekbrains',
         )
-        superuser.set_password('geekbrains')
-        superuser.is_superuser = True
-        superuser.is_staff = True
-        superuser.save()
-        for i in range(count):
-            user = User.objects.create(
-                username=f'uname{i}',
-                first_name=f'fname{i}',
-                last_name=f'lname{i}',
-                email=f'user{i}@gb.local')
-            print(f'author {user.first_name} created')
-        print('done')
+
+        print(f'Суперпользователь {superuser.username} создан')
+
+        self.stdout.write("Creating new users...")
+        people = []
+        for _ in range(count):
+            person = UserFactory()
+            people.append(person)
+            print(f'{person} создан')
+        print('Done!')
