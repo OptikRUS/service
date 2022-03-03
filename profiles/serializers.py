4e1e686059a16ab51c6bf2bc7e1e199ae4009b95
optikrus <optikrus61@gmail.com>
2022-03-03 18:33:03 +0300

    edit Project & Todo visualization serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,16 +1,33 @@
 from rest_framework.serializers import ModelSerializer
 
 from users.models import User
+from users.serializers import UserModelSerializer
 from profiles.models import Project, ToDo
 
 
+class UserListModelSerializer(UserModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+
+class UserTodoListModelSerializer(UserModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', )
+
+
 class ProjectModelSerializer(ModelSerializer):
+    users = UserListModelSerializer(many=True)
+
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = ('name', 'url', 'created_at', 'users')
 
 
 class ToDoModelSerializer(ModelSerializer):
+    user = UserTodoListModelSerializer()
+
     class Meta:
         model = ToDo
-        fields = ('name', 'user', 'text', 'update_at', 'is_active')
+        exclude = ('id',)
