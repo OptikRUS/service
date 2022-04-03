52b91279700cec16659246d6c995687d6c2d3704
optikrus <optikrus61@gmail.com>
2022-04-03 20:32:09 +0300

    add swagger & redoc

--- service/settings.py
+++ service/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'django_filters',
+    'drf_yasg',
 
     'users',
     'profiles',
--- service/urls.py
+++ service/urls.py
@@ -18,11 +18,21 @@ from django.urls import path, include, re_path
 
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from users.views import UserModelViewSet
 from profiles.views import ProjectModelViewSet, ToDoModelViewSet
 
+schema_view = get_schema_view(openapi.Info(
+    title="Library",
+    default_version='0.1',
+    description="Documentation to Library project",
+    contact=openapi.Contact(email="django@gb.local"),
+    license=openapi.License(name="MIT License"), ),
+    public=True, permission_classes=(AllowAny,), )
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
@@ -37,4 +47,8 @@ urlpatterns = [
     path('api-token-auth/', obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
