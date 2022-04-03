428de07f690b5fb8b26c13859c190fb4a4b80ac8
optikrus <optikrus61@gmail.com>
2022-04-03 19:48:04 +0300

    add v2 for users

--- service/settings.py
+++ service/settings.py
@@ -151,6 +151,8 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ],
 
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
 
