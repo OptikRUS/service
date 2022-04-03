49430043f46d8085658fa0994176c5b0a95ff1b0
optikrus <optikrus61@gmail.com>
2022-04-03 14:59:56 +0300

    add profiles tests to APITestCase

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,6 +1,6 @@
-from django.test import TestCase
+import json
 from rest_framework import status
-from rest_framework.test import APIRequestFactory, force_authenticate, APIClient
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APITestCase
 from mixer.backend.django import mixer
 from django.contrib.auth.models import User
 
@@ -9,7 +9,7 @@ from users.models import User
 from profiles.models import Project, ToDo
 
 
-class TestProjectModelViewSet(TestCase):
+class TestProjectModelViewSet(APITestCase):
     def setUp(self):
         self.superuser = User.objects.create_superuser('django', 'django@gb.local', 'geekbrains')
         self.user = User.objects.create_user('test_user', 'test_user_fp@gb.local', 'geekbrains')
@@ -21,45 +21,39 @@ class TestProjectModelViewSet(TestCase):
 
     """List projects"""
     def test_get_list_guest(self):
-        factory = APIRequestFactory()
-        request = factory.get('/api/projects/')
-        view = ProjectModelViewSet.as_view({'get': 'list'})
-        response = view(request)
+        response = self.client.get('/api/projects/')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_list_auth(self):
-        factory = APIRequestFactory()
-        request = factory.get('/api/projects/')
-        force_authenticate(request, user=self.user)
-        view = ProjectModelViewSet.as_view({'get': 'list'})
-        response = view(request)
+        self.client.login(username='test_user', password='geekbrains')
+        response = self.client.get('/api/projects/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_list_admin(self):
-        factory = APIRequestFactory()
-        request = factory.get('/api/projects/')
-        force_authenticate(request, user=self.superuser)
-        view = ProjectModelViewSet.as_view({'get': 'list'})
-        response = view(request)
+        self.client.login(username='django', password='geekbrains')
+        response = self.client.get('/api/projects/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     """Detail projects"""
     def test_get_detail_project_for_guest(self):
-        client = APIClient()
-        response = client.get(f'/api/projects/{self.project.id}/')
+        response = self.client.get(f'/api/projects/{self.project.id}/')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_detail_project_for_user(self):
-        client = APIClient()
-        client.force_authenticate(user=self.user)
-        response = client.get(f'/api/projects/{self.project.id}/')
+        self.client.force_login(user=self.user)
+        response = self.client.get(f'/api/projects/{self.project.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_project = json.loads(response.content)
+        self.assertEqual(response_project['name'], 'test_project_name')
+        self.assertEqual(response_project['repoUrl'], 'https://www.test.github.com/')
 
     def test_get_detail_project_for_admin(self):
-        client = APIClient()
-        client.force_authenticate(user=self.superuser)
-        response = client.get(f'/api/projects/{self.project.id}/')
+        self.client.force_login(user=self.superuser)
+        response = self.client.get(f'/api/projects/{self.project.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_project = json.loads(response.content)
+        self.assertEqual(response_project['name'], 'test_project_name')
+        self.assertEqual(response_project['repoUrl'], 'https://www.test.github.com/')
 
     """Create projects"""
     def test_create_project_for_guest(self):
@@ -110,39 +104,34 @@ class TestProjectModelViewSet(TestCase):
 
     """Edit projects"""
     def test_edit_project_for_guest(self):
-        client = APIClient()
-        response = client.put(f'/api/projects/{self.project.id}/', self.project_data_upd)
+        response = self.client.put(f'/api/projects/{self.project.id}/', self.project_data_upd)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_edit_project_for_admin(self):
         project = mixer.blend(Project)
-        client = APIClient()
-        client.login(username='django', password='geekbrains')
-        response = client.put(f'/api/projects/{project.id}/', self.project_data_upd, format='json')
+        self.client.force_login(user=self.superuser)
+        response = self.client.put(f'/api/projects/{project.id}/', self.project_data_upd, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         project = Project.objects.get(id=project.id)
         self.assertEqual(project.name, self.project_data_upd['name'])
         self.assertEqual(project.repo_url, self.project_data_upd['repo_url'])
-        client.logout()
 
     def test_edit_project_for_user(self):
         project = mixer.blend(Project)
-        client = APIClient()
-        client.login(username='test_user', password='geekbrains')
-        response = client.put(f'/api/projects/{project.id}/', self.project_data_upd, format='json')
+        self.client.force_login(user=self.user)
+        response = self.client.put(f'/api/projects/{project.id}/', self.project_data_upd, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         project = Project.objects.get(id=project.id)
         self.assertEqual(project.name, self.project_data_upd['name'])
         self.assertEqual(project.repo_url, self.project_data_upd['repo_url'])
-        client.logout()
 
 
-class TestToDoModelViewSet(TestCase):
+class TestToDoModelViewSet(APITestCase):
     def setUp(self):
         self.superuser = User.objects.create_superuser('django', 'django@gb.local', 'geekbrains')
         self.user = User.objects.create_user('test_user', 'test_user@gb.local', 'geekbrains')
         self.project = Project.objects.create(name='test_project_name', repo_url='https://www.test.github.com/')
-        self.todo = ToDo.objects.create(name='test_todo_name_name', text='test_text', is_active=1,
+        self.todo = ToDo.objects.create(name='test_todo_name', text='test_text', is_active=1,
                                         project=self.project, user=self.user)
         self.todo_data = {'name': 'test_todo_name', 'text': 'test_todo_text', 'is_active': 1,
                           'project': f"http://127.0.0.1:8000/api/projects/{self.project.id}/",
@@ -151,26 +140,17 @@ class TestToDoModelViewSet(TestCase):
 
     """List todos"""
     def test_get_list_guest(self):
-        factory = APIRequestFactory()
-        request = factory.get('/api/todos/')
-        view = ToDoModelViewSet.as_view({'get': 'list'})
-        response = view(request)
+        response = self.client.get('/api/todos/')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_list_auth(self):
-        factory = APIRequestFactory()
-        request = factory.get('/api/users/')
-        force_authenticate(request, user=self.user)
-        view = ToDoModelViewSet.as_view({'get': 'list'})
-        response = view(request)
+        self.client.login(username='test_user', password='geekbrains')
+        response = self.client.get('/api/todos/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_list_admin(self):
-        factory = APIRequestFactory()
-        request = factory.get('/api/users/')
-        force_authenticate(request, user=self.superuser)
-        view = ToDoModelViewSet.as_view({'get': 'list'})
-        response = view(request)
+        self.client.login(username='django', password='geekbrains')
+        response = self.client.get('/api/todos/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     """Detail todos"""
@@ -180,16 +160,20 @@ class TestToDoModelViewSet(TestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_detail_todo_for_user(self):
-        client = APIClient()
-        client.force_authenticate(user=self.user)
-        response = client.get(f'/api/todos/{self.todo.id}/')
+        self.client.force_login(user=self.user)
+        response = self.client.get(f'/api/todos/{self.todo.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_todo = json.loads(response.content)
+        self.assertEqual(response_todo['name'], 'test_todo_name')
+        self.assertEqual(response_todo['text'], 'test_text')
 
     def test_get_detail_todo_for_admin(self):
-        client = APIClient()
-        client.force_authenticate(user=self.superuser)
-        response = client.get(f'/api/todos/{self.todo.id}/')
+        self.client.force_login(user=self.superuser)
+        response = self.client.get(f'/api/todos/{self.todo.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_todo = json.loads(response.content)
+        self.assertEqual(response_todo['name'], 'test_todo_name')
+        self.assertEqual(response_todo['text'], 'test_text')
 
     """Create todos"""
     def test_create_todo_for_guest(self):
@@ -218,55 +202,46 @@ class TestToDoModelViewSet(TestCase):
     """Edit todos"""
     def test_edit_todo_for_guest(self):
         client = APIClient()
-        response = client.put(f'/api/todos/{self.todo.id}/', self.todo_data_upd)
+        response = client.patch(f'/api/todos/{self.todo.id}/', self.todo_data_upd)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_edit_todo_for_user(self):
         todo = mixer.blend(ToDo)
-        client = APIClient()
-        client.login(username='test_user', password='geekbrains')
-        response = client.patch(f'/api/todos/{todo.id}/', self.todo_data_upd)
+        self.client.force_login(user=self.user)
+        response = self.client.patch(f'/api/todos/{todo.id}/', self.todo_data_upd)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         todo = ToDo.objects.get(id=todo.id)
         self.assertEqual(todo.name, self.todo_data_upd['name'])
         self.assertEqual(todo.text, self.todo_data_upd['text'])
         self.assertEqual(todo.is_active, self.todo_data_upd['is_active'])
-        client.logout()
 
     def test_edit_todo_for_admin(self):
         todo = mixer.blend(ToDo)
-        client = APIClient()
-        client.login(username='django', password='geekbrains')
-        response = client.patch(f'/api/todos/{todo.id}/', self.todo_data_upd)
+        self.client.force_login(user=self.superuser)
+        response = self.client.patch(f'/api/todos/{todo.id}/', self.todo_data_upd)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         todo = ToDo.objects.get(id=todo.id)
         self.assertEqual(todo.name, self.todo_data_upd['name'])
         self.assertEqual(todo.text, self.todo_data_upd['text'])
         self.assertEqual(todo.is_active, self.todo_data_upd['is_active'])
-        client.logout()
 
     """Delete todos"""
     def test_delete_todo_for_guest(self):
-        client = APIClient()
-        response = client.delete(f'/api/todos/{self.todo.id}/')
+        response = self.client.delete(f'/api/todos/{self.todo.id}/')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_delete_todo_for_user(self):
         todo = mixer.blend(ToDo)
-        client = APIClient()
-        client.login(username='test_user', password='geekbrains')
-        response = client.delete(f'/api/todos/{todo.id}/')
+        self.client.force_login(user=self.user)
+        response = self.client.delete(f'/api/todos/{todo.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         todo = ToDo.objects.get(id=todo.id)
         self.assertEqual(todo.is_active, 0)
-        client.logout()
 
     def test_delete_todo_for_admin(self):
         todo = mixer.blend(ToDo)
-        client = APIClient()
-        client.login(username='django', password='geekbrains')
-        response = client.delete(f'/api/todos/{todo.id}/')
+        self.client.force_login(user=self.superuser)
+        response = self.client.delete(f'/api/todos/{todo.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         todo = ToDo.objects.get(id=todo.id)
         self.assertEqual(todo.is_active, 0)
-        client.logout()
