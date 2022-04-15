d8e0dcbde1b8382fcc5ca2aa397edbe9fc124ec9
optikrus <optikrus61@gmail.com>
2022-04-15 23:52:55 +0300

    add frontend on docker

--- service/settings.py
+++ service/settings.py
@@ -62,10 +62,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'service.urls'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost",
+#     "http://127.0.0.1",
+# ]
 
 TEMPLATES = [
     {
